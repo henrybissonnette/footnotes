ab1cacd350f35f025f82858d3f4ff7daa9120dc7
Henry Bissonnette <henrydbissonnette@gmail.com>
2012-09-27 09:04:36 -0500

    overlay reload triggers on change rather than load of iframe

--- app/assets/javascripts/overlay.js
+++ app/assets/javascripts/overlay.js
@@ -2,7 +2,7 @@
 
 $(document).ready(function(){
   var ignore_first = 1
-  $('#focal_item').load(function(){
+  $('#focal_item').change(function(){
     ignore_first -= 1
     if (ignore_first){
       window.location.replace(this.contentWindow.location.href.replace('overlay/proxy?','overlay?'))
