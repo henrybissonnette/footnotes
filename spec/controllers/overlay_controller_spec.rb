8ab17728ba0492913d5bcdb6f2ff5e0bc618f592
Henry Bissonnette <henrydbissonnette@gmail.com>
2012-10-22 09:13:00 -0500

    coffeeized overlay.js

--- spec/controllers/overlay_controller_spec.rb
+++ spec/controllers/overlay_controller_spec.rb
@@ -1,24 +1,6 @@
 require 'spec_helper'
 
 describe OverlayController do
-  # describe 'localize_link' do
-  #   before do
-  #     #@controller = OverlayController.new
-  #     pre_string = '/overlay/proxy?from_click=true&external_url='
-  #   end
-
-  #   it 'should prepend the local string to complete urls' do
-  #     complete_urls = [
-  #       'www.other.site.com',
-  #       'naked.url.4.info/this/that/',
-  #       'http://www.website.org/a/b/c/d',
-  #       'https://www.can-have-hyphens-and-numb3rs.io/indian/ocean'
-  #     ]
-
-  #     complete_urls.each do |url|
-  #       @controller.localize_link(url,url).should == url + pre_string
-  #     end
-  #   end
 
   describe 'GET #view' do
     before do
